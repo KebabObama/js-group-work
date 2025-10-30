ab8eaf51b9af82d4e6e169df2c74e21650a7d449
KebabObama <lucaschyba@gmail.com>
2025-10-30 12:40:03 +0100

    add sidebar on keypress

--- modules/sidebar.js
+++ modules/sidebar.js
@@ -2,13 +2,12 @@
  * Sidebar initialization
  * @param {string} s_query
  * @param {string} t_query
+ * @param {string} action
  */
-export const sidebar = (s_query, t_query) => {
+export const sidebar = (s_query, t_query, action = "b") => {
   const sidebar_element = document.querySelector(s_query);
   const toggle_elements = document.querySelectorAll(t_query);
-  toggle_elements.forEach((e) => {
-    e.onclick = () => {
-      sidebar_element.classList.toggle("sidebar--active");
-    };
-  });
+  const tgl = () => sidebar_element.classList.toggle("sidebar--active");
+  document.onkeydown = (e) => e.key === action && tgl();
+  toggle_elements.forEach((e) => e.onclick = tgl);
 };
